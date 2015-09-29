e7eb9c04918040dac524db86a4b4b4b0b0a04e11
Rachael Blake <blake@nceas.ucsb.edu>
2015-09-29 12:38:13 -0700

    rearranged and organized

--- Spartina Bmss_MesoExpt 2013.R
+++ Spartina Bmss_MesoExpt 2013.R
@@ -32,7 +32,32 @@ detach(SP)
 
 #write.csv(HgtMean,"C:\\Users\\rblake\\Documents\\LSU\\MesoExp_2013\\Sp Biomass\\SpBmss_Meso2013.csv")
 
+###################################################################
+library(ggplot2) ; library(plyr) ; library(grid) ; library(scales)
 
+#######################################
+### MAKE MY OWN THEME TO SAVE LINES OF CODE
+theme_boxplot <- function(base_size = 12){
+ theme_bw(base_size) %+replace%
+   theme(legend.key.size=unit(15,"points"),
+         legend.text=element_text(size=I(13)),
+         legend.key=element_blank(),
+         legend.title=element_blank(),
+         legend.position="none",
+         plot.margin=unit(c(0.25,2,0.25,2), "lines"), # respectively: top, right, bottom, left; refers to margin *outside* labels; default is c(1,1,0.5,0.5)
+         panel.border=element_rect(colour='black', fill = NA),
+         panel.margin=unit(0,"lines"),
+         axis.ticks.length=unit(1,"mm"),
+         axis.ticks.margin = unit(0, "lines"),
+         axis.text=element_text(size=15),
+         axis.title.x=element_text(hjust=.55, vjust=-.01, size=17),
+         axis.title.y=element_text(size=17, angle=90, hjust=.56, vjust=-.001),
+         panel.grid.major=element_blank(),
+         panel.grid.minor=element_blank(),
+         strip.text.x=element_text(size=14),
+         strip.background=element_rect(colour='black', fill='white'))
+}  
+##########################################
 
 ######################################
 ## Using the mean data
@@ -167,37 +192,9 @@ Wk8PlotS <- ggplot(data=Wk8, aes(x=Herbivore, y=TtlStemNum, fill=Chem1)) +
                            panel.background=element_blank(),plot.background=element_blank()) +
                      scale_fill_manual(values=colors, guide=guide_legend(title = NULL))
 
-###################################################################################################
 
-head(Stems)
-
-library(ggplot2) ; library(plyr) ; library(grid) ; library(scales)
-
-#######################################
-### MAKE MY OWN THEME TO SAVE LINES OF CODE
-theme_boxplot <- function(base_size = 12){
- theme_bw(base_size) %+replace%
-   theme(legend.key.size=unit(15,"points"),
-         legend.text=element_text(size=I(13)),
-         legend.key=element_blank(),
-         legend.title=element_blank(),
-         legend.position="none",
-         plot.margin=unit(c(0.25,2,0.25,2), "lines"), # respectively: top, right, bottom, left; refers to margin *outside* labels; default is c(1,1,0.5,0.5)
-         panel.border=element_rect(colour='black', fill = NA),
-         panel.margin=unit(0,"lines"),
-         axis.ticks.length=unit(1,"mm"),
-         axis.ticks.margin = unit(0, "lines"),
-         axis.text=element_text(size=15),
-         axis.title.x=element_text(hjust=.55, vjust=-.01, size=17),
-         axis.title.y=element_text(size=17, angle=90, hjust=.56, vjust=-.001),
-         panel.grid.major=element_blank(),
-         panel.grid.minor=element_blank(),
-         strip.text.x=element_text(size=14),
-         strip.background=element_rect(colour='black', fill='white'))
-}  
-##########################################
 ##################################################################################################
-##ABOVEGROUND BIOMASS
+## ABOVEGROUND BIOMASS
 ##########################################
 PMass <- read.csv("SpBmss_Mass_MesoExpt 2013.csv")
 head(PMass)
@@ -283,7 +280,7 @@ LiveStem <- ggplot(data=LiveP, aes(x=Herbivore, y=Dry_Wgt, fill=Chem1)) +
                      scale_fill_manual(values=colors, guide=guide_legend(title = NULL))
 
 
-################################################
+#########################################################################################
 ## BELOWGROUND BIOMASS
 ################################################
 setwd("C:\\Users\\rblake\\Documents\\LSU\\MesoExp_2013\\Sp Biomass")  
@@ -412,7 +409,7 @@ setwd("C:\\Users\\rblake\\Documents\\LSU\\MesoExp_2013\\")
 ALLDATA <- read.csv("ALL DATA_SEM_MesoExpt2013.csv")
 # Below is from: https://stat.ethz.ch/pipermail/r-help/2011-November/295230.html
 # Fit a linear model to the data and save the model object:
-modD <- lm(LogDdRootDryWgt~LogDeadStemDryWgt, data=ALLDATA)
+modD <- lm(LogDeadStemDryWgt~LogDdRootDryWgt, data=ALLDATA)
 # Create a list of character strings - the first component
 # produces the fitted model, the second produces a
 # string to compute R^2, but in plotmath syntax.
@@ -420,7 +417,7 @@ routD <- list(paste('Fitted model:',round(coef(modD)[1],3),' + ',
                    round(coef(modD)[2],3), 'x',sep = ''),
              paste('R^2 == ',round(summary(modD)[['r.squared']],3),
                    sep=''))
-Dd <- ggplot(ALLDATA, aes(LogDeadStemDryWgt, LogDdRootDryWgt)) + geom_point() +
+Dd <- ggplot(ALLDATA, aes(LogDdRootDryWgt,LogDeadStemDryWgt)) + geom_point() +
              geom_smooth(method=lm) + #theme_boxplot() + 
              geom_text(aes(x=.5, y=.8, label=routD[[2]]), 
                        hjust=0, parse=TRUE)
