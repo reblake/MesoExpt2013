e600adcd523914cd220fe873932914c60a5bae07
Rachael Blake <blake@nceas.ucsb.edu>
2016-04-18 18:14:24 -0700

    fixed stressor calc function

--- Multi_Stress_Calc/Expected Stressor Effects Function.r
+++ Multi_Stress_Calc/Expected Stressor Effects Function.r
@@ -4,11 +4,11 @@
 ### Rachael E. Blake    March 2014            ###
 #################################################
 
-library(tidyr) ; library(plyr) ; library(dplyr)
+library(tidyr) ; library(plyr) ; library(dplyr) ; library(ggplot2) ; library(scales)
 
 # All data
 AllD <- read.csv("C:/Users/rblake/Documents/LSU/MesoExp_2013/ALL_DATA_SEM_MesoExpt2013.csv")
-AllD$Chem1 <- factor(AllD$Chem, levels=c('NC', 'Core', 'Oil', 'OilCore'))
+AllD$Chem1 <- factor(AllD$Chem, levels=c('NC', 'Oil', 'Core', 'OilCore'))
 
 # LIVE2 <- AllD %>%
 #          select(Bucket, Chem, LiveStemDryWgt_g) %>%
@@ -24,30 +24,34 @@ AllD$Chem1 <- factor(AllD$Chem, levels=c('NC', 'Core', 'Oil', 'OilCore'))
 LIVE <- read.csv("C:/Users/rblake/Documents/LSU/MesoExp_2013/Exp_Stress_LiveBmss_MesoExpt2013.csv")
 boxplot(LIVE)
 ggplot(AllD, aes(x=Chem1, y=LiveStemDryWgt_g)) + stat_summary(fun.y="mean", geom="bar")
+LIVEmn <- summarise_each(LIVE, funs(mean))
 
 # Dead Stem Biomass
 DEAD <- read.csv("C:/Users/rblake/Documents/LSU/MesoExp_2013/Exp_Stress_DeadBmss_MesoExpt2013.csv")
 boxplot(DEAD)
 ggplot(AllD, aes(x=Chem1, y=DeadStemDryWgt_g)) + stat_summary(fun.y="mean", geom="bar")
-DEADmn <- 
+DEADmn <- summarise_each(DEAD, funs(mean))
 
 # Stem Number
 NUM <- read.csv("C:/Users/rblake/Documents/LSU/MesoExp_2013/Exp_Stress_StemNum_MesoExpt2013.csv")
 boxplot(NUM)
 ggplot(AllD, aes(x=Chem1, y=TtlStemNum)) + stat_summary(fun.y="mean", geom="bar")
+NUMmn <- summarise_each(NUM, funs(mean))
 
 # Prokelisia
 PROK <- read.csv("C:/Users/rblake/Documents/LSU/MesoExp_2013/Exp_Stress_Insects_MesoExpt2013.csv")
 boxplot(PROK)
 ggplot(AllD, aes(x=Chem1, y=ProkAbunScaled)) + stat_summary(fun.y="mean", geom="bar")
+PROKmn <- summarise_each(PROK, funs(mean(., na.rm=TRUE)))
 
 # Snails
 SNAL <- read.csv("C:/Users/rblake/Documents/LSU/MesoExp_2013/Exp_Stress_Snails_MesoExpt2013.csv")
 boxplot(SNAL)
 ggplot(AllD, aes(x=Chem1, y=SnailWgtScaled)) + stat_summary(fun.y="mean", geom="bar")
+SNALmn <- summarise_each(SNAL, funs(mean(., na.rm=TRUE)))
 
 # make a list of the data frames
-expstrlist <- list(DEAD, LIVE, NUM, PROK, SNAL)
+expstrlist <- list(LIVEmn, DEADmn, NUMmn, PROKmn, SNALmn)
 
 ###################################################
 # Expected Stressor Effects Function Code: (Additive)
@@ -66,8 +70,20 @@ ExpectedStress <- function(dafr) {
 #    3 = mixed1 (oil>control)
 #    4 = mixed2 (oil<control)      
        
-ExpectedStress(LIVE)
+ExpectedStress(LIVEmn)
 
 ####################################################
 
 # use lapply to run my fuction for each of the datasets
+
+
+
+
+
+
+
+
+
+
+
+
