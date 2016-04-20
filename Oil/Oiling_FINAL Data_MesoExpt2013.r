82704dbe92a78a277051e85555474f3e8c71690a
Rachael Blake <blake@nceas.ucsb.edu>
2016-04-19 18:29:17 -0700

    edited theme_boxplot

--- Oil/Oiling_FINAL Data_MesoExpt2013.r
+++ Oil/Oiling_FINAL Data_MesoExpt2013.r
@@ -24,23 +24,22 @@ theme_boxplot <- function(base_size = 12){
           legend.key=element_blank(),
           legend.title=element_blank(),
           legend.position="none",
-          plot.margin=unit(c(0.25,2,0.25,2), "lines"), # respectively: top, right, bottom, left; refers to margin *outside* labels; default is c(1,1,0.5,0.5)
-          panel.border=element_rect(colour='black', fill = NA),
+          plot.margin=unit(c(0.5,1,0.5,1), "lines"), # respectively: top, right, bottom, left; refers to margin *outside* labels; default is c(1,1,0.5,0.5)
+          panel.border=element_blank(),
           panel.margin=unit(0,"lines"),
           axis.ticks.length=unit(1,"mm"),
-          axis.text.x = element_text(margin=margin(0,0,0,0, "lines")),
-          axis.text.y = element_text(margin=margin(0,0,0,0,"lines")),
-          axis.text=element_text(size=15),
-          axis.title.x=element_text(hjust=.55, vjust=-.01, size=17),
-          axis.title.y=element_text(size=17, angle=90, hjust=.56, vjust=-.001),
+          axis.text.x = element_text(margin=margin(5,0,0,0)),
+          axis.text.y = element_text(margin=margin(0,5,0,0)),
+          axis.text=element_text(size=13),
+          axis.title.x=element_text(size=17, margin=margin(15,0,0,0)), 
+          axis.title.y=element_text(size=15, angle=90, margin=margin(0,15,0,0)), 
           panel.grid.major=element_blank(),
           panel.grid.minor=element_blank(),
           strip.text.x=element_text(size=14),
           strip.background=element_rect(colour='black', fill='white'),
           axis.line.x = element_line(colour = 'black', size=0.5, linetype='solid'),
           axis.line.y = element_line(colour = 'black', size=0.5, linetype='solid'))
-}  
-
+}   
 ##########################################
 ##########
 
