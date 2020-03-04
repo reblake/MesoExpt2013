77afb36820830539823f6da04f1f0913578c8d49
Rachael Blake <rblake@sesync.org>
2020-03-04 15:30:22 -0500

    updated file paths and some attribute tables

--- Write_Attributes_KNB.R
+++ Write_Attributes_KNB.R
@@ -23,34 +23,65 @@ write_xlsx(attributes, "./Attributes_LICOR_PhotosynMeas.xlsx")
 
 ###
 
-AllDat_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Attributes_Data_Publication/Attributes_ALL_DATA_SEM.csv", stringsAsFactors = FALSE)
+AllDat_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Attributes_ALL_DATA_SEM.csv", stringsAsFactors = FALSE)
 shiny_attributes(attributes = AllDat_attr)
 
-OilConc_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Attributes_Data_Publication/Attributes_OilConc.csv", stringsAsFactors = FALSE)
+OilConc_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Attributes_OilConc.csv", stringsAsFactors = FALSE)
 shiny_attributes(attributes = OilConc_attr)
 
-Sulfide_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Attributes_Data_Publication/Attributes_Sulfide.csv", stringsAsFactors = FALSE)
+Sulfide_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Attributes_Sulfide.csv", stringsAsFactors = FALSE)
 shiny_attributes(attributes = Sulfide_attr)
 
-Isotopes_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Attributes_Data_Publication/Attributes_Isotopes.csv", stringsAsFactors = FALSE)
+Isotopes_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Attributes_Isotopes.csv", stringsAsFactors = FALSE)
 shiny_attributes(attributes = Isotopes_attr)
 
-Temp_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Attributes_Data_Publication/Attributes_Temp_Data_MASTER.csv", stringsAsFactors = FALSE)
+Temp_attr <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Attributes_Temp_Data_MASTER.csv", stringsAsFactors = FALSE)
 shiny_attributes(attributes = Temp_attr)
 
-###
+##################################
 # From https://nceas.github.io/datateam-training/training/editing-eml.html
 # Public: http://training.arcticdata.io/materials/arctic-data-center-training/programming-metadata-and-data-publishing.html
 
 cn <- CNode('PROD')
 knb <- getMNode(cn, "urn:node:KNB")
 
-urn_uuid <- "urn:uuid:9fb92ec3-fd9c-46ee-91d6-345886cf287b"
+urn_uuid <- "urn:uuid:63454430-40b2-4334-88d0-3859e5924443"
 
 pkg <- get_package(knb, urn_uuid, file_names = TRUE) # lists out basic metdata for objects in package
 dat_files <- read_eml(getObject(knb, pkg$metadata))  # lists out the metadata for data files/figures in package
 
-# make updates to EML document using EML package
+# updates EXISTING EML document using EML package
+# set attributes
+AllDat_attr_list <- set_attributes(AllDat_attr)
+OilConc_attr_list <- set_attributes(OilConc_attr)
+Sulfide_attr_list <- set_attributes(Sulfide_attr)
+Isotopes_attr_list <- set_attributes(Isotopes_attr)
+Temp_attr_list <- set_attributes(Temp_attr)
+
+# set custom units
+custom_units <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Custom_Units_Sulfide.csv", stringsAsFactors = FALSE)
+custom_unitList <- set_unitList(custom_units, as_metadata = TRUE)
+
+old_ulist <- dat_files$additionalMetadata$metadata$unitList
+dat_files$additionalMetadata$metadata$unitList <- c(old_ulist, custom_unitList)
+test <- c(old_ulist, custom_unitList)
+
+dat_files$dataset$otherEntity[[1]]$attributeList <- Temp_attr_list
+dat_files$dataset$otherEntity[[2]]$attributeList <- Isotopes_attr_list
+dat_files$dataset$otherEntity[[3]]$attributeList <- Sulfide_attr_list
+dat_files$dataset$otherEntity[[4]]$attributeList <- OilConc_attr_list
+dat_files$dataset$otherEntity[[5]]$attributeList <- AllDat_attr_list
+
+
+eml_validate(dat_files) 
+
+write_eml(dat_files, "Spartina_data_eml.xml")
+
+# publish!
+pkg <- publish_update(knb, pkg$metadata, pkg$resource_map, pkg$data, metadata_path = "Spartina_data_eml.xml")
+
+###################################################
+# makes NEW  EML document using EML package
 # should be functionalized but for now not
 AllDat_attr_list <- set_attributes(AllDat_attr)
 AllDat_attr_phys <- set_physical("ALL_DATA_SEM_MesoExpt2013.csv")
@@ -83,16 +114,16 @@ Temp_dataTable <- list(entityName = "Temp_Data_MASTER_MesoExpt2013.csv", attribu
                           physical = Temp_attr_phys)
 
 # set custom units
-custom_units <- read.csv("D:/Documents/LSU/MesoExp_2013/Attributes_Data_Publication/Custom_Units_Sulfide.csv", stringsAsFactors = FALSE)
+custom_units <- read.csv("D:/Documents/LSU/MesoExp_2013/Analysis/Attributes_Data_Publication/Custom_Units_Sulfide.csv", stringsAsFactors = FALSE)
 
-custom_unitList <- set_unitList(custom_units)
+custom_unitList <- set_unitList(custom_units, as_metadata = TRUE)
 
 # add attributes metadata to package
 data_pkg <- list(dataTable = c(AllDat_dataTable, OilConc_dataTable, Sulfide_dataTable,
                                Isotope_dataTable, Temp_dataTable))
 
 data_eml <- list(packageId = "urn:uuid:9fb92ec3-fd9c-46ee-91d6-345886cf287b", system = "knb",
-                 dataset = data_pkg, 
+                 dataset = list(dataTable = data_pkg), 
                  additionalMetadata = list(metadata = list(unitList = custom_unitList)))
 
 write_eml(data_eml, "Spartina_data_eml.xml")
